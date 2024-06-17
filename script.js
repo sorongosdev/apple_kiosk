482337406a2616eb471796946e2c543e93e3fe86
Sorongos <dosora9@gmail.com>
2024-06-18 05:02:31 +0900

    3nm 버튼 클릭 이벤트 추가

---
+++ script.js
@@ -0,0 +1,23 @@
+document.querySelector(".sub-3nm-btn").addEventListener("click", function () {
+  // 텍스트 표시
+  const sub_3nm_overlay_txt = this.querySelector(".sub-3nm-overlay-txt");
+  sub_3nm_overlay_txt.classList.add("show");
+
+  const sub_3nm_sub_txt = this.querySelector(".sub-3nm-sub-txt");
+  const sub_3nm_main_txt = this.querySelector(".sub-3nm-main-txt");
+
+  // 텍스트 숨기기
+  sub_3nm_sub_txt.classList.add("hidden");
+  sub_3nm_main_txt.classList.add("hidden");
+
+  // 배경을 어둡게 만들기
+  this.classList.add("darkened");
+
+  // 3초 후 원래 상태로 되돌리기
+  setTimeout(() => {
+    sub_3nm_overlay_txt.classList.remove("show");
+    sub_3nm_sub_txt.classList.remove("hidden");
+    sub_3nm_main_txt.classList.remove("hidden");
+    this.classList.remove("darkened");
+  }, 3000); // 3000ms = 3초
+});
